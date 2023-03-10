0c909473c404cefd302fc362ed0b538c78569c23
Fabio Migliaccio <febbio9@gmail.com>
2023-03-10 11:53:49 +0100

    header branch create

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,14 @@
+import { Button } from '@mui/material'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+    <Button variant="contained">
+					<Link to="/dashboard/loans">Accedeixxx</Link>
+				</Button>
+                </>
   )
 }
 
