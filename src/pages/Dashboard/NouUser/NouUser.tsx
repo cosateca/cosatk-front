2fdb36be410cf563647fadcfea3c2e29eec8c763
Daniel <usechedaniel88@gmail.com>
2023-03-10 10:33:46 +0100

    users medio hecha

---
+++ src/pages/Dashboard/NouUser/NouUser.tsx
@@ -0,0 +1,29 @@
+import { Box, Container, Typography } from '@mui/material'
+import React from 'react'
+import Navbar from '../../../components/Navbar/Navbar'
+
+type Props = {}
+
+const NouUser = (props: Props) => {
+	return (
+		<>
+			<Box display={{ xs: 'block', sm: 'flex' }} overflow-y={{ xs: 'hidden' }}>
+				<Navbar />
+				<section>
+					<Container
+						sx={{
+							padding: { xs: '25px', sm: '50px' },
+							width: '100vw',
+							height: '100vh',
+						}}
+					>
+						<Typography variant="h1">USUARIS</Typography>
+						Users, aqui dentro va el contenido a desarrollar por cada developer
+					</Container>
+				</section>
+			</Box>
+		</>
+	)
+}
+
+export default NouUser
