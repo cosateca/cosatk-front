5740e43429b7a3766d3a22af5a34a5aac20de896
Fabio Migliaccio <febbio9@gmail.com>
2023-03-13 12:10:41 +0100

    mobile version style add checkbox

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,188 +1,189 @@
 
-import { Box, Typography, TextField, Button, Grid } from '@mui/material'
+import { Box, Typography, TextField, Button, Grid, Checkbox } from '@mui/material'
 import { height } from '@mui/system'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Footer from '../../components/Footer/Footer'
 
 const Register = () => {
-  return (
-    <>
-    
-    <Box  
+    return (
+        <>
+
+            <Box
                 sx={{
                     margin: '0 auto',
                     display: 'flex',
                     flexDirection: 'column',
                     alignItems: 'center',
                     width: { xs: '80%', sm: '20%' },
-                    marginTop : 8,
+                    marginTop: 8,
                     height: '800px',
-                    overflow:'scroll',
-                    
-                    
-                    
+                    overflow: 'scroll',
+
+
+
                 }}
             >
-                 <Typography variant="h1" sx = {{ margin: 5 , fontWeight: 'light'}}> Register </Typography> 
-                
-            
-                    <TextField
-                        id="Nom"                 
-                        label="Nom"
-                        variant="outlined"
-                        fullWidth
-                        margin="normal"
-                        InputLabelProps={{
-                            style: {
-                                color: '#222222',
-                            },
-                        }}
-                    />
-                    <TextField
-                        id="Cognom"                 
-                        label="Cognom"
-                        variant="outlined"
-                        fullWidth
-                        margin="normal"
-                        InputLabelProps={{
-                            style: {
-                                color: '#222222',
-                            },
-                        }}
-                    />
-                    <TextField
-                        id="DNI/NIE"                 
-                        label="DNI/NIE"
-                        variant="outlined"
-                        margin="normal"
-                        fullWidth
-                        
-                        InputLabelProps={{
-                            style: {
-                                color: '#222222',
-                                
-                                
-                            },
-                        }}
-                    />
-                    <TextField
-                        id="Direcció"                 
-                        label="Direcció"
-                        variant="outlined"
-                        margin="normal"
-                        fullWidth
-                        InputLabelProps={{
-                            style: {
-                                color: '#222222',
-                            },
-                        }}
-                    />
-                    <TextField
-                        id="Télefon"                 
-                        label="Teléfon"
-                        variant="outlined"
-                        fullWidth
-                        margin="normal"
-                        InputLabelProps={{
-                            style: {
-                                color: '#222222',
-                            },
-                        }}
-                    />
-                    <TextField
-                        id="Any de naixement"                 
-                        label="Any de naixement"
-                        variant="outlined"
-                        fullWidth
-                        margin="normal"
-                        InputLabelProps={{
-                            style: {
-                                color: '#222222',
-                            },
-                        }}
-                    />
-                    <TextField
-                        id="Email"                 
-                        label="Email"
-                        variant="outlined"
-                        fullWidth
-                        margin="normal"
-                        InputLabelProps={{
-                            style: {
-                                color: '#222222',
-                            },
-                        }}
-                    />
+                <Typography variant="h1" sx={{ margin: 5, fontWeight: 'light' }}> Register </Typography>
 
-<TextField
-                        id="EmaPasswordil"                 
-                        label="Password"
-                        variant="outlined"
-                        fullWidth
-                        margin="normal"
-                        InputLabelProps={{
-                            style: {
-                                color: '#222222',
-                            },
-                        }}
-                    />
 
-                    <TextField
-                        
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="Repetir password"
-                        label="Repetir password"
-                        
-                        id="password"
-                        InputLabelProps={{
-                            style: {
-                                color: '#222222',
-                            
-                            },
-                        }}
-                        
+                <TextField
+                    id="Nom"
+                    label="Nom"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    InputLabelProps={{
+                        style: {
+                            color: '#222222',
+                        },
+                    }}
+                />
+                <TextField
+                    id="Cognom"
+                    label="Cognom"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    InputLabelProps={{
+                        style: {
+                            color: '#222222',
+                        },
+                    }}
+                />
+                <TextField
+                    id="DNI/NIE"
+                    label="DNI/NIE"
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
 
-                      
+                    InputLabelProps={{
+                        style: {
+                            color: '#222222',
 
-                    />
-                    {/* <FormControlLabel
+
+                        },
+                    }}
+                />
+                <TextField
+                    id="Direcció"
+                    label="Direcció"
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                    InputLabelProps={{
+                        style: {
+                            color: '#222222',
+                        },
+                    }}
+                />
+                <TextField
+                    id="Télefon"
+                    label="Teléfon"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    InputLabelProps={{
+                        style: {
+                            color: '#222222',
+                        },
+                    }}
+                />
+                <TextField
+                    id="Any de naixement"
+                    label="Any de naixement"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    InputLabelProps={{
+                        style: {
+                            color: '#222222',
+                        },
+                    }}
+                />
+                <TextField
+                    id="Email"
+                    label="Email"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    InputLabelProps={{
+                        style: {
+                            color: '#222222',
+                        },
+                    }}
+                />
+
+                <TextField
+                    id="EmaPasswordil"
+                    label="Password"
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    InputLabelProps={{
+                        style: {
+                            color: '#222222',
+                        },
+                    }}
+                />
+
+                <TextField
+
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="Repetir password"
+                    label="Repetir password"
+
+                    id="password"
+                    InputLabelProps={{
+                        style: {
+                            color: '#222222',
+
+                        },
+                    }}
+
+
+
+
+                />
+                {/* <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
               label="Remember me"
             /> */}
-                    <Button
-                        type="submit"
-                        
-                        variant="contained"
-                        sx={{
-                            mt: 3,
-                            mb: 2,
-                            borderRadius: '0px',
-                            bgcolor: '',
-                            color: 'white',
-                            width: {xs: '100%', sm: '50%'}
-                            
-                
+                <Button
+                    type="submit"
+
+                    variant="contained"
+                    sx={{
+                        mt: 3,
+                        mb: 2,
+                        borderRadius: '0px',
+                        bgcolor: '',
+                        color: 'white',
+                        width: { xs: '100%', sm: '50%' }
+
+                    }}
+
+                >
+                    Register
+                </Button>
+
+                <p>  <Checkbox />  He llegit i accepto els
+                    termes i condicions i la
+                    política de privacitat.</p>
+                    <p> Tornar</p>
 
 
-                        }}
 
-                    >
-                        Register
-                    </Button>
-                    
+                <Footer />
+            </Box>
 
-                   
 
-                    <Footer/>
-                </Box>
 
-   
-    
-    </>
-  )
+        </>
+    )
 }
 
 export default Register
