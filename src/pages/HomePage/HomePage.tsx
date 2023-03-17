054cbeea87557a1a91e4411227992e2a6cd6d856
Daniel <usechedaniel88@gmail.com>
2023-03-17 08:56:16 +0100

    home con imagen iconos

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,20 +1,68 @@
-import { Container, Button } from '@mui/material'
-import { Link } from 'react-router-dom'
+/* eslint-disable no-mixed-spaces-and-tabs */
+import { Container, Button, Box } from '@mui/material'
+
 
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
+import CardProduct from '../../components/CardProduct/CardProduct'
+import image from 'material-ui/svg-icons/image/image'
+import logo from '../../assets/images/image_home_page.png'
+
 
 
 // type Props = {}
 
 const HomePage = ( ) => {
 	return (
-		<Container>
-
-			<Header/>				
+		<>
+			<Header/>
+			<Box
+        		component="img"
+        		sx={{
+					width: '100%',
+          			height: '100%',
+        		}}
+        		alt="The house from the offer."
+        		src={logo}
+      		/>
+			{/* <Box 
+								sx={{
+									backgroundImage:'url("../../assets/images/logo.png")',
+									width: '1200px',
+									height: '200px'
+								}}
+			>
+			</Box> */}
+			<Container>
+				<h2>ARTICLES POPULARS</h2>
+					<Box
+					sx={{
+						display: 'flex',
+						flexDirection: { xs: 'column', sm: 'row' },
+						gap: '20px',
+					}}
+					>
+						<CardProduct/>
+						<CardProduct/>
+						<CardProduct/>
+						<CardProduct/>
+					</Box>
+				<h2>ARTICLES POPULARS</h2>
+				<Box
+				sx={{
+					display: 'flex',
+					flexDirection: { xs: 'column', sm: 'row' },
+					gap: '20px',
+				}}
+				>
+					<CardProduct/>
+					<CardProduct/>
+					<CardProduct/>
+					<CardProduct/>
+				</Box>				
+			</Container>		
 			<Footer/>
-
-		</Container>
+		</>
 	)
 }
 
