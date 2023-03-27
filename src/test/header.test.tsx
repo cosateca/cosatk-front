e108d0849056ea03a04de5ef8d1c60d7205ba13f
Freddy Vegas <vegasfalexis@gmail.com>
2023-03-27 11:57:09 +0200

    of new testing

---
+++ src/test/header.test.tsx
@@ -0,0 +1,55 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react"
+import Header from '../../src/components/Header/Header'
+import Loans from '../../src/pages/Dashboard/Loans/Loans'
+import React from 'react'
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import userEvent from '@testing-library/user-event'
+
+
+let Typography:any;
+let Button:any;
+describe('NavBar', ()=>{
+    beforeEach(()=>{
+        render(
+        <BrowserRouter>
+            <Header/>
+        </BrowserRouter>
+        )
+        Typography = screen.getByText(/Accedeix/i)
+        Button = screen.getByRole('button', {name:'Accedeix'});
+    })
+    it('render appropritoateley', ()=>{
+        expect(Typography).toBeInTheDocument()
+    })
+    it('It should show the loan screen when the loan button is clicked', async ()=>{
+      render( 
+            <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Loans />} />
+            </Routes> 
+            </BrowserRouter>
+        )
+        // console.log(screen.getAllByText(''))
+        await userEvent.click(screen.getByRole('button', {name:'Accedeix'}));
+        screen.debug()
+        expect(Button).toBeInTheDocument()
+    })
+    it('It should not show the loan screen, when the article is clicked', async ()=>{
+        render( 
+              <BrowserRouter>
+              <Routes>
+                  <Route path="/" element={<Loans />} />
+              </Routes> 
+              </BrowserRouter>
+          )
+          // console.log(screen.getAllByText(''))
+
+        //   await userEvent.click(screen.getAllByText('Articles')[0]);
+        //   screen.debug()
+        //   expect(screen.queryByText(/PRÈSTECS/)).not.toBeInTheDocument()
+
+          
+          // await screen.findByText('ssss')
+      })
+})
---
+++ src/test/navbar.test.tsx
@@ -0,0 +1,58 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react"
+import NavBar from '../../src/components/Navbar/Navbar'
+import Loans from '../../src/pages/Dashboard/Loans/Loans'
+import React from 'react'
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import userEvent from '@testing-library/user-event'
+
+
+let Typography:any;
+describe('NavBar', ()=>{
+    beforeEach(()=>{
+        render(
+        <BrowserRouter>
+        <NavBar/>
+        </BrowserRouter>
+        )
+        Typography = screen.getByText(/TANCAR SESSIÓ/i)
+    })
+    it('render appropritoateley', ()=>{
+        expect(Typography).toBeInTheDocument()
+    })
+    it('It should show the loan screen when the loan button is clicked', async ()=>{
+      render( 
+            <BrowserRouter>
+            <Routes>
+                {/* <Link to='/dashboard/loans'>
+                </Link> */}
+                <Route path="/" element={<Loans />} />
+            </Routes> 
+            </BrowserRouter>
+        )
+        // console.log(screen.getAllByText(''))
+        await userEvent.click(screen.getAllByText('Prèstecs')[0]);
+        screen.debug()
+        expect(screen.getByText(/PRÈSTECS/)).toBeInTheDocument()
+        // await screen.findByText('ssss')
+    })
+    it('It should not show the loan screen, when the article is clicked', async ()=>{
+        render( 
+              <BrowserRouter>
+              <Routes>
+                  {/* <Link to='/dashboard/loans'>
+                  </Link> */}
+                  <Route path="/" element={<Loans />} />
+              </Routes> 
+              </BrowserRouter>
+          )
+          // console.log(screen.getAllByText(''))
+
+        //   await userEvent.click(screen.getAllByText('Articles')[0]);
+        //   screen.debug()
+
+          expect(screen.queryByText(/PRÈSTECS/)).not.toBeInTheDocument()
+          // await screen.findByText('ssss')
+      })
+
+})
